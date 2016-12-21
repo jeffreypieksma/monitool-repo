5dfdc7bc73d4a2cacacfa58464351a3a05943667
Jeffrey Pieksma <jefreypieksma@hotmail.com>
2016-12-21 13:44:18 +0100

    dashboard js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,65 @@
 $( document ).ready(function() { 
 
+  //Read more link
+
+  var showChar = 200;
+
+    var ellipsestext = "...";
+
+    var moretext = "Lees meer";
+
+   var lesstext = "Lees minder";    $('.more').each(function() {
+
+       var content = $(this).html();        if(content.length > showChar) {
+
+
+
+           var c = content.substr(0, showChar);
+
+           var h = content.substr(showChar, content.length - showChar);
+
+
+
+           var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+
+
+           $(this).html(html);
+
+       }
+
+
+
+   });
+
+
+
+   $(".morelink").click(function(){
+
+       if($(this).hasClass("less")) {
+
+           $(this).removeClass("less");
+
+           $(this).html(moretext);
+
+       } else {
+
+           $(this).addClass("less");
+
+           $(this).html(lesstext);
+
+       }
+
+       $(this).parent().prev().toggle();
+
+       $(this).prev().toggle();
+
+       return false;
+
+   });
+
+  //Filters
+
 	var filterbutton = $( ".mainfilter" );
   var formbutton = $( "#formbutton" );
   var filterwrapper = $( ".filterwrapper" ).css("display","none");
